f45a96e6e02c827a2936bdc132152b03480f0d3c
Abdullah Waseem <abdullah.waseem@ontariotechu.net>
2023-11-16 23:31:17 -0500

    innital commit

---
+++ client/components/layout/Footer.vue
@@ -0,0 +1,29 @@
+<template functional>
+    <footer class="footer section__footer">
+        <div class="footer__content">
+            <div class="footer__left">
+                <span>Copyright &copy; Abdullah Waseem</span>
+            </div>
+            <div class="footer__right">
+                <span>Github</span>
+                <a
+                    href="https://github.com/abdullahwaseem01/chat-application"
+                    target="_blank"
+                    class="nav__link"
+                >
+                    <ion-icon name="logo-github" class="footer__icon"></ion-icon>
+                </a>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+};
+</script>
+
+<style lang="scss">
+@import '@/assets/scss/components/footer.scss';
+</style>
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Chat App
+
+A chat application that allows users from all over the world to connect and chat with each other in rooms. Users can create rooms, delete rooms, users can join and leave rooms. Users can make private and public rooms. Users have profiles associated with them meaning all their rooms and conversations are saved. Conversations are real time. 
+
+## How to Run 
+```
+git clone https://github.com/abdullahwaseem01/Chat-Application.git 
+cd chat-app
+cd client
+npm install
+cd ..
+cd serverad
+npm install
+cd ..
+npm run dev
+```
+## Features
+- [x] Realtime Chat
+- [x] Private Chat
+- [x] Group Chat
+- [x] User Authentication
+- [x] User Profile
+- [x] User Status
